cc90ca5d61b179980483b4bbdb89faf124fc2e1a
karan chauhan <karan.chauahn.btech2023@sitpune.edu.in>
2025-03-04 23:21:48 +0530

    User input

--- Students/Main.java
+++ Students/Main.java
@@ -1,4 +1,42 @@
 import java.util.* ;
 
 class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations() ;
+        String choice = "Yes";
+        do {
+        System.out.println("Enter Name :  ");
+        String name = scan.nextLine();
+
+        System.out.println("Enter PRN :  ");
+        long prn = Long.parseLong(scan.nextLine());
+
+        System.out.println("Enter GPA :  ");
+        double gpa = Double.parseDouble(scan.nextLine());
+
+        System.out.println("Enter Batch :  ");
+        String batch = scan.nextLine();
+
+        System.out.println("Enter Branch :  ");
+        String branch = scan.nextLine();
+
+        System.out.println("Do you want to Continue(Yes/No) :  ");
+        choice = scan.nextLine();
+        }while(!choice.equalsIgnoreCase("No"));
+        System.out.println("Enter Name :  ");
+        String name = scan.nextLine();
+
+        System.out.println("Enter PRN :  ");
+        long prn = Long.parseLong(scan.nextLine());
+
+        System.out.println("Enter GPA :  ");
+        double gpa = Double.parseDouble(scan.nextLine());
+
+        System.out.println("Enter Batch :  ");
+        String batch = scan.nextLine();
+
+        System.out.println("Enter Branch :  ");
+        String branch = scan.nextLine();
+
 }
